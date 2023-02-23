7f6395832661ceeb7c4ea65085b66688820263e9
seanhot5 <sean.hot5@gmail.com>
2023-02-23 17:58:13 -0500

    -Added MailHelper Method to access AngusMail API

---
+++ app/src/main/java/com/example/securecmail/MailHelper.java
@@ -0,0 +1,135 @@
+package com.example.securecmail;
+
+import android.util.Log;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Folder;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Store;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+public class MailHelper {
+
+    public void receiveMail(String hostInput, String user, String pass) throws MessagingException {
+        new Thread(){
+            public void run() {
+
+                String host = resolveHostToIPV4(hostInput);
+
+                Properties props = System.getProperties();
+                props.put("mail.debug", "true");
+                props.put("mail.imap.host", host);
+                props.put("mail.imap.ssl.enable", "true");
+                props.put("mail.imap.port", "993");
+                props.put("mail.imap.auth", "true");
+                props.put("mail.imap.ssl.checkserveridentity", "false");
+                props.put("mail.imap.ssl.trust", "*");
+
+                Authenticator auth = new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(user, pass);
+                    }
+                };
+
+                try {
+                    Session imapSession = Session.getInstance(props, auth);
+                    imapSession.setDebug(true);
+                    Store store = imapSession.getStore("imap");
+                    store.connect(host, user, pass);
+                    Folder inbox = store.getFolder("Inbox");
+                    inbox.open(Folder.READ_WRITE);
+                    Message[] messages = inbox.getMessages();
+                    for (int i = 1; i < 11; i++) {
+                        Log.d("Email Number " + i, messages[messages.length-i].getSubject());//outputs "i" most recent emails from inbox
+                        //get all messages with SecureCMail Header -- SecureCMail and maybe a timestamp in subject, some confirmation in body before share 1?
+                        //keep user info stored persistently, keep user email accounts info in encrypted shared prefs?
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }//thread bracket
+        }.start();
+    }
+
+    public void sendMail(String hostInput, String user, String pass){
+        new Thread(){
+            public void run() {
+
+                String host = resolveHostToIPV4(hostInput);
+
+                boolean sessionDebug = true;
+                String to = "sean.hot5@gmail.com";
+                String from = "sean.hot5@gmail.com";
+                String subject = "SecureCMail";
+                String messageText = "Share 1";
+
+                Properties props = System.getProperties();
+                //props.put("mail.smtp.ehlo", "false");
+                props.put("mail.debug", "true");
+                props.put("mail.smtp.host", host);
+                props.put("mail.smtp.ssl.enable", "true");
+                props.put("mail.smtp.port", "465");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.ssl.checkserveridentity", "false"); //maybe figure out how to fix this
+                props.put("mail.smtp.ssl.trust", "*");
+
+                Authenticator auth = new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(user, pass);
+                    }
+                };
+
+                try {
+                    Session mailSession = Session.getDefaultInstance(props, auth);
+                    mailSession.setDebug(sessionDebug);
+                    Message msg = new MimeMessage(mailSession);
+                    msg.setFrom(new InternetAddress(from));
+                    InternetAddress[] address = {new InternetAddress(to)};
+                    msg.setRecipients(Message.RecipientType.TO, address);
+                    msg.setSubject(subject);
+                    msg.setSentDate(new Date());
+                    msg.setText(messageText);
+                    Transport transport = mailSession.getTransport("smtp");
+                    transport.connect(host, user, pass);
+                    transport.sendMessage(msg, msg.getAllRecipients());
+                    transport.close();
+                    System.out.println("Message sent successfully!");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public String resolveHostToIPV4(String host){
+        InetAddress hostINets[] = null;
+        String hostIPV4 = null;
+        try {
+            hostINets = InetAddress.getAllByName("smtp.gmail.com");
+            System.out.println("Host inet is: "+hostINets);
+            for(int i =0;i<hostINets.length;i++){
+                if(hostINets[i] instanceof Inet4Address){
+                    hostIPV4 = hostINets[i].getHostAddress();
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return hostIPV4;
+    }
+
+
+}
--- app/src/main/java/com/example/securecmail/MessageActivity.java
+++ app/src/main/java/com/example/securecmail/MessageActivity.java
@@ -5,13 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import jakarta.mail.MessagingException;
+
 
 public class MessageActivity extends AppCompatActivity {
 
+    EditText input_subject, input_body;
+    Button send_button, attachment_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        input_subject = findViewById(R.id.input_subject);
+        input_body = findViewById(R.id.input_body);
+        send_button = findViewById(R.id.send_button);
+        attachment_button = findViewById(R.id.attachment_button);
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg_subject = input_subject.toString();
+                String msg_body = input_body.toString();
+                System.out.println("Entering Send Message Method...");
+                MailHelper mailHelper = new MailHelper();
+                String user = "";
+                String pass = "";
+                String host = "smtp.gmail.com";
+                mailHelper.sendMail(host, user, pass);
+            }
+        });
+
+        attachment_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MailHelper mailHelper = new MailHelper();
+                String user = "";
+                String pass = "";
+                String host = "imap.gmail.com";
+                try {
+                    mailHelper.receiveMail(host, user, pass);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     /**
@@ -23,4 +61,5 @@ public class MessageActivity extends AppCompatActivity {
         startActivity(intent);
 
     }//end Back
+
 }

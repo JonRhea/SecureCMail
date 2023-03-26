f6b87ea73c2bae6309b580956be2f0095b732cbd
Jon Rhea <indianarhea@gmail.com>
2023-03-26 00:28:26 -0400

    - Contacts now appear in a ListView in ContactActivity - Changed gilda display font to sans-serif in ContactActivity -- The above change is temporary. There is a bug in Android Studio where custom fonts prevent layout previews from rendering. - Will go back for clean up very soon

--- app/src/main/java/com/example/securecmail/ContactActivity.java
+++ app/src/main/java/com/example/securecmail/ContactActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -24,10 +27,14 @@ public class ContactActivity extends AppCompatActivity {
     EditText contactEmail1;
     EditText contactEmail2;
 
+    ListView contactListView;
+
     String uniqueName;
     String fullName;
     String emailId1;
     String emailId2;
+
+    //ArrayList<String> contactList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,9 @@ public class ContactActivity extends AppCompatActivity {
         contactFullName = findViewById(R.id.input_full_name);
         contactEmail1 = findViewById(R.id.input_email1);
         contactEmail2 = findViewById(R.id.input_email2);
+        contactListView = findViewById(R.id.contact_list);
+
+        refreshContact();
 
         //temporarily deleting contact_info.txt every run for testing
         //File filePath = new File(ContactActivity.this.getFilesDir(), "securecmail_data");
@@ -46,6 +56,17 @@ public class ContactActivity extends AppCompatActivity {
         //}//end if
 
     }
+
+    /**
+     * Refreshed the ListView to update list of contacts
+     */
+    public void refreshContact(){
+        ArrayList<String> contactList = new ArrayList<String>();
+        readContact(contactList);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, contactList);
+        contactListView.setAdapter(adapter);
+    }//end refreshContact
 
     /**
      * Save a contact's info
@@ -72,7 +93,8 @@ public class ContactActivity extends AppCompatActivity {
                 outputStream.close();
 
                 System.out.println("New Contact was saved successfully!");
-                readContactTest();
+                refreshContact();
+                //readContact();
             }//end try
             catch(FileNotFoundException e){
                 Toast.makeText(this, "Error: File Not Found",Toast.LENGTH_LONG).show();
@@ -110,9 +132,12 @@ public class ContactActivity extends AppCompatActivity {
         return true;
     }//end checkContact
 
-    public void readContactTest(){
+    /**
+     * Reads in all the contacts from the contact_info.txt file
+     * @param contactList The array list to store contact info in to.
+     */
+    public void readContact(ArrayList<String>contactList){
 
-        String contactString = "";
         File filePath = new File(ContactActivity.this.getFilesDir(), "securecmail_data");
         if(!filePath.exists()){
             filePath.mkdir();
@@ -126,6 +151,7 @@ public class ContactActivity extends AppCompatActivity {
             String data;
             while(bufferedReader.ready()){
                 data = bufferedReader.readLine();
+                contactList.add(data);
                 System.out.println(data);
             }//end while
             inputStream.close();
